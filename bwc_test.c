b708552a96e722f8652d901c70d2095b55346c58
Nicolas Sauzede <nicolas.sauzede@gmail.com>
2025-07-04 17:46:12 +0200

    Working food eating

--- bwc_test.c
+++ bwc_test.c
@@ -9,11 +9,8 @@ typedef struct {
         f_y:3, f_x:3,
         len:6,
         h_y:3, h_x:3,
-        dir:44
+        dir:44  // 22 dirs, len <=23
     ;
-    // 22 dirs
-// 3,4 E => 3,4 3,5
-//    int len;
 } state_t;
 typedef enum {N,E,S,W} dir_t;
 #define WW 8
@@ -30,9 +27,11 @@ uint64_t state_to_data(state_t state) {
     union { state_t state; uint64_t data; } u = { state };
     return u.data;
 }
-void draw_(state_t state) {
+void draw(uint64_t data) {
+    state_t state = data_to_state(data);
     char screen[WW*HH];
     memset(screen, '.', WW*HH);
+    screen[state.f_x + WW*state.f_y] = 'F';
     screen[state.h_x + WW*state.h_y] = 'H';
     uint64_t dir_ = state.dir;
     int len = state.len;
@@ -60,7 +59,6 @@ void draw_(state_t state) {
         screen[d_x + WW*d_y] = 'a' + i - 1;
         dir_ >>= 2;
     }
-    screen[state.f_x + WW*state.f_y] = 'f';
     int s = sizeof(state);
     //EXPECT_EQ(4, s, "sizeof(state_t)=%d", s);
 #if 1
@@ -75,7 +73,8 @@ void draw_(state_t state) {
         printf("\n");
     }
 }
-state_t evolve_(state_t state) {
+uint64_t evolve(uint64_t data) {
+    state_t state = data_to_state(data);
     int d_x = state.h_x, d_y = state.h_y;
     uint64_t dir_ = state.dir;
     int dir = dir_ & 0x3;
@@ -92,17 +91,16 @@ state_t evolve_(state_t state) {
         if (d_x <= 0) d_x = WW;
         d_x--;
     }
+    if ((d_x == state.f_x) && (d_y == state.f_y)) {
+        if (state.len < 23) {
+            state.len++;
+        }
+        //state.f_x = 0;
+        //state.f_y = 0;
+        state.f_x = (state.f_x + 1) % 8;
+    }
     state.h_x = d_x; state.h_y = d_y;
     state.dir = (dir_ << 2) | dir;
-    return state;
-}
-void draw(uint64_t data) {
-    state_t state = data_to_state(data);
-    draw_(state);
-}
-uint64_t evolve(uint64_t data) {
-    state_t state = data_to_state(data);
-    state = evolve_(state);
     data = state_to_data(state);
     return data;
 }
@@ -111,50 +109,21 @@ TESTMETHOD(test2) {
     state_t state = {
         .f_y=3, .f_x=5,
         .len=2,
-        .h_y=4, .h_x=4,
+        .h_y=3, .h_x=3,
         .dir=0x00000000005
     };
     uint64_t data = state_to_data(state);
     printf("\n");
-    for (int n = 0; n < 3; n++) {
+    for (int n = 0; n < 8; n++) {
         draw(data);
         data = evolve(data);
     }
 }
-/*
-........
-.tuv....
-lsrqponm
-kdefgfij
-.cbaH...
-........
-........
-........
-
-........
-........
-........
-........
-....Habc
-kjihgfed
-lmnopqrs
-.....vut
-
-........
-........
-........
-........
-....vuts
-klmnopqr
-jihgfedc
-.....Hab
-*/
 TESTMETHOD(test1) {
     state_t state = {
         .f_y=3, .f_x=5,
         .len=23,
         .h_y=7, .h_x=5,
-    //.dir=0xf9555bfff95
       .dir=0x56fffe5556f
     };
     uint64_t data = state_to_data(state);
